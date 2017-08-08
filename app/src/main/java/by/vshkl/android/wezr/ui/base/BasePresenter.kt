340292efa706ee96e482589358f2ba09ad69ac69
Pavel Vashkel <vshkl@protonmail.ch>
2017-08-08 21:05:26 +0300

    Update main presenter

--- app/src/main/java/by/vshkl/android/wezr/ui/base/BasePresenter.kt
+++ app/src/main/java/by/vshkl/android/wezr/ui/base/BasePresenter.kt
@@ -1,13 +1,13 @@
 package by.vshkl.android.wezr.ui.base
 
-import rx.Subscription
-import rx.subscriptions.CompositeSubscription
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
-open class BasePresenter<T: MvpView>: Presenter<T> {
+open class BasePresenter<T : MvpView> : Presenter<T> {
 
     var mvpView: T? = null
         private set
-    private val compositeSubscription = CompositeSubscription()
+    private val compositeSubscription = CompositeDisposable()
 
     override fun attachView(mvpView: T) {
         this.mvpView = mvpView
@@ -15,21 +15,23 @@ open class BasePresenter<T: MvpView>: Presenter<T> {
 
     override fun detachView() {
         mvpView = null
-        if (!compositeSubscription.isUnsubscribed) compositeSubscription.clear()
+        if (!compositeSubscription.isDisposed) {
+            compositeSubscription.clear()
+        }
     }
 
     val isViewAttached: Boolean
         get() = mvpView != null
 
     fun checkIsViewAttached() {
-        if (!isViewAttached) throw MvpViewNotAttachedException()
+        if (!isViewAttached) {
+            throw MvpViewNotAttachedException()
+        }
     }
 
-    fun addSubscription(sub: Subscription) {
-        compositeSubscription.add(sub)
+    fun addSubscription(disposable: Disposable) {
+        compositeSubscription.add(disposable)
     }
 
     private class MvpViewNotAttachedException internal constructor() : RuntimeException("Please call Presenter.attachView(MvpView) before" + " requesting data to the Presenter")
-
-
 }
